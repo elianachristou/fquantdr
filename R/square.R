ab1ba774c84a7cb37ecd96eba4ef3123a950b5e2
Eliana Christou <67390194+elianachristou@users.noreply.github.com>
2024-05-22 15:19:31 -0400

    Delete R/square.R

--- R/square.R
+++
@@ -1,8 +0,0 @@
-square <- function(x) {
-  # Square a number
-  res_square <- x ^ 2
-  # Take square root of a number
-  res_sqrt <- sqrt(x)
-  # Return results
-  list(res_square = res_square, res_sqrt = res_sqrt)
-}
