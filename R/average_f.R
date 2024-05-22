aa19b0d8944f4b6012a89e789fa57cd023342f4e
Eliana Christou <echris15@uncc.edu>
2024-05-22 14:57:56 -0400

    added the sum of the squares in the average_f function

--- R/average_f.R
+++ R/average_f.R
@@ -1,3 +1,7 @@
 average_f <- function(x) {
-  print(sum(x) / length(x))
+  # this finds the average of x
+  output1 <- sum(x) / length(x)
+  # this finds the sum of the squared values divide by n
+  output2 <- sum(x^2) / length(x)
+  list(output1 = output1, output2 = output2)
 }
