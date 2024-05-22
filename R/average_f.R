669e00151cbd1682b71e62350aa85f09093432ca
Eliana Christou <echris15@uncc.edu>
2024-05-22 14:59:10 -0400

    added the variance

--- R/average_f.R
+++ R/average_f.R
@@ -3,5 +3,7 @@ average_f <- function(x) {
   output1 <- sum(x) / length(x)
   # this finds the sum of the squared values divide by n
   output2 <- sum(x^2) / length(x)
-  list(output1 = output1, output2 = output2)
+  # this calculates the variance
+  output3 <- sum(x - output1)^2 / (length(x) - 1)
+  list(output1 = output1, output2 = output2, output3 = output3)
 }
