3b1f29aa90589b8c5e84aefe0ee51a4882bb97ca
Eliana Christou <echris15@uncc.edu>
2024-06-19 22:05:01 +0300

    modify title and description

--- R/gen_funct_data.R
+++ R/gen_funct_data.R
@@ -1,13 +1,16 @@
-#' function that generates noiseless functional data using Fourier Basis functions
+#' Functional data
 #'
-#' \code{gen_funct_data} generates noiseless functional data using Fourier
-#' basis functions
+#' \code{fundata} generates functional data based on Fourier basis functions
 #'
-#' This function initializes arrays for the original and centered functional
-#' predictors and construct Fourier basis functions over the interval [0, 1].
-#' For each predictor, it computes the functional data by multiplying the
-#' relevant scores with the evaluated basis functions and then centers this data
-#' by subtracting the mean of each time point across all observation.
+#' This function generates functional data for a given number of observations
+#' ('n'), functional predictors ('p'), basis functions ('q'), and times points
+#' ('t').  It utilizes a specified coefficient matrix ('eta') to create the
+#' functional predictors.  Using these basis functions, it computes the
+#' original functional predictors ('g') by multiplying the coefficients
+#' with the basis functions. The function then centers these functional
+#' predictors ('cg') by subtracting the mean across observations for each
+#' time point.  The output is a list containing both the original and centered
+#' functional predictors.
 #'
 #' @param n Sample size, i.e., the number of observations
 #' @param p the number of predictors
