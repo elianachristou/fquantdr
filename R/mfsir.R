657d4a2857bd2169d163c0016b1e31820ef0cd3b
Noah <noahpfd23@gmail.com>
2024-06-20 08:38:08 -0400

    Addition of mfsir description.

--- R/mfsir.R
+++ R/mfsir.R
@@ -1,6 +1,11 @@
 #' mfsir: FSIR that can incorporate multivariate functional predictor
 
-#' \code{mfsir} description
+#' \code{mfsir}
+
+#' This function performs Multivariate Functional Sliced Inverse
+#' Regression (mfsir) on a given 3-dimensional data array and
+#' response vector.
+
 
 #' This code (description)
 
@@ -17,15 +22,17 @@
 #' @noRd
 
 #' @examples
-#'
+#' mfsir(Xc, y, H, nbasis)
 
-
+#
 mfsir <- function(Xc, y, H, nbasis) {
   n <- dim(Xc)[1]
   p <- dim(Xc)[3]
-
+  #
   databasis <- create.bspline.basis(rangeval = c(0, 1), nbasis = nbasis)
+  #
   xfd.coef <- numeric() #retrieves the coodinates as an n x (p * q) matrix
+  #
   for (k in 1:p) {
     xfdk <- fda::smooth.basis(t, t(Xc[, , k]), databasis)$fd
     xfdk <- fda::center.fd(xfdk)
@@ -35,32 +42,52 @@ mfsir <- function(Xc, y, H, nbasis) {
 
   #compute block diagonal gram matrix gx
   gi <- gramatrix(nbasis, databasis)
-
+  #
   gx <- matrix(0, nrow = p * (nbasis), ncol = p * (nbasis))
+  #
   for (i in 1:p) {
     index <- ((i - 1) * (nbasis) + 1):(i * (nbasis))
     gx[index, index] <- gi
   }
-
+  #
   sigmaxx <- (t(xfd.coef) %*% xfd.coef) / n
+  #
   gx.sqrt <- mppower(gx, 1/2, 10e-7)
+  #
   store <- gx.sqrt %*% sigmaxx %*% gx.sqrt
+  #
   store.inv.sqrt <- rigpower(store, -1/2, 0.05)
+  #
   mucoef <- apply(xfd.coef, 2, mean)
+  #
   ydis <- discretize (y, 1:H)
+  #
   prob <- slprob(ydis, 1:H)
+  #
   avg.slav <- slav(xfd.coef, ydis, 1:H)
+  #
   Lambda1 <- matrix(0, p * nbasis, p * nbasis)
+  #
   for(i in 1:H) {
     Lambda1 <- Lambda1 + prob[i] * avg.slav[i, ] %*% t(avg.slav[i, ])}
+  #
   sigmaxxy <- Lambda1 - mucoef %*% (t(mucoef))
+  #
   mat <- store.inv.sqrt %*% gx.sqrt
+  #
   out <- mat %*% sigmaxxy %*% t(mat)
+  #
   out <- symmetry(out)
+  #
   eigendecom <- eigen(out)
+  #
   eigvalues <- eigendecom$values
+  #
   phi <- eigendecom$vec
+  #
   gx.inv.sqrt <- rigpower(gx, -1/2, 0.05)
+  #
   betas <- gx.inv.sqrt %*% store.inv.sqrt %*% phi
+  #
   return(list(phi = phi, betas = betas, eigvalues = eigvalues, xfd.coef = xfd.coef, gx = gx))
 }
