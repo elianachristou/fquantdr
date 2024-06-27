a41e1be6d98171954070bdd4960e4afa1a88c72f
Eliana Christou <echris15@uncc.edu>
2024-06-27 11:13:24 +0300

    update the example to match the one from matpower functio

--- R/mppower.R
+++ R/mppower.R
@@ -23,8 +23,9 @@
 #'
 #' @noRd
 #' @examples
-#' mat <- matrix(c(4, 1, 1, 3), 2, 2)
-#' result <- mppower(mat, 2)
+#' mat <- matrix(c(6, 4, 8, 2, 5, 9, 3, 1, 7), nrow = 3, ncol = 3)
+#' alpha <- 2
+#' result <- mppower(mat, alpha)
 #'
 mppower <- function(a, alpha, epsilon = 0, ignore = 10^(-15)) {
   # Checks if 'a' is a matrix
