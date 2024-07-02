34f79bc2b763af022033ca830882c2309ba795e3
Eliana Christou <echris15@uncc.edu>
2024-07-02 12:46:05 +0300

    fix some documentation and spacing

--- R/sonf.R
+++ R/sonf.R
@@ -2,17 +2,17 @@
 #'
 #' \code{sonf} estimates the relationship between a scalar response and
 #' functional predictors using basis functions, with options for applying
-#' a second derivative penalty or not. Test
+#' a second derivative penalty or not.
 #'
 #' This function performs scalar-on-function regression, predicting a scalar
 #' response \code{y} using one or more functional predictors \code{xfd}. It
-#' centralizes the data, constructs a design matrix based on the different type
-#' of basis functions, and calculates the regression coefficients with or without
-#' a second derivative penalty. The function returns the regression coefficients,
-#' functional data objects for these coefficients, estimated response value,
-#' and the designed matrix X.
+#' centralizes the data, constructs a design matrix based on the different
+#' type of basis functions, and calculates the regression coefficients with
+#' or without a second derivative penalty. The function returns the regression
+#' coefficients, functional data objects for these coefficients, estimated
+#' response value, and the designed matrix X.
 #'
-#' @param y A number vector of length n, representing the scalar response variable
+#' @param y A numeric vector of length n, representing the scalar response.
 #' @param xfd A functional data object
 #' @param dev2_penalty A logical flag indicating whether to apply a second
 #'    derivative penalty.
@@ -21,11 +21,9 @@
 #' @return \code{sonf} computes a scalar-on-function regression and returns:
 #'    \itemize{
 #'    \item \code{beta_coef}: The regression coefficients.
-#'
-#'    \item \code{beta_fd}: The functional data objects for the beta coefficients.
-#'
+#'    \item \code{beta_fd}: The functional data objects for the beta
+#'    coefficients.
 #'    \item \code{yhat}: The predicted values of \code{y}.
-#'
 #'    \item \code{X}: The designed matrix used in the regression.
 #'    }
 #'
@@ -34,26 +32,32 @@ sonf = function(y, xfd, dev2_penalty = FALSE, lambda=NULL) {
 
   y <- as.matrix(y) # double check!
 
-  # compatibility checks
-  # checks if y is univariate
+  # Check if y is univariate response
   if (dim(y)[2] > 1) {
-    stop(paste("y needs to be a univariate response. y is a", dim(y)[2], "-dimensional response in this case."))
+    stop(paste("y needs to be a univariate response. y is a",
+               dim(y)[2], "-dimensional response in this case."))
   }
-  # checks if xfd is multivariate
+
+  # Check if xfd is a functional object of class 'fd'
   if (!inherits(xfd, "fd")) {
     stop("xfd must be a functional data object of class 'fd'")
   }
-  # checks if the coefficients of xfd is an array
+
+  # Check if xfd is an array
   if (length(dim(xfd$coef)) != 3) {
     stop("xfd$coef must be an array with dimensions (nbasis, n, p)")
   }
-  # checks if the number of observations for xfd and y agree
+
+  # Check if the number of observations for xfd and y agree
   if (length(y) != dim(xfd$coef)[2]) {
     stop("The length of y and the number of observations in xfd must agree")
   }
-  # checks if n > p
+
+  # Check if n > p
   if (length(y) <= dim(xfd$coef)[3]) {
-    stop(paste("number of observations of y (", length(y), ") should be greater than the number of predictors of xfd (", dim(xfd$coef)[3], ").", sep = ""))
+    stop(paste("number of observations of y (", length(y), ") should be
+               greater than the number of predictors of xfd
+               (", dim(xfd$coef)[3], ").", sep = ""))
   }
 
   basis <- xfd$basis
@@ -73,17 +77,20 @@ sonf = function(y, xfd, dev2_penalty = FALSE, lambda=NULL) {
   p <- tmp[3]
   if(is.na(p)) p <- 1
 
-  if (!is.numeric(p) || p <= 0) {
-    stop("The number of functional predictors (p) must be a positive integer")
+  if (p != floor(p) | p <= 0) {
+    stop("The number of functional predictors (p) must be a positive
+         integer.")
   }
 
   if (p == 1) {
     xcoef <- t(as.matrix(xfd$coef[, , 1])) # because originally it is an array
   } else {
     # block diagonal of B - p times
-    B <- Matrix::bdiag(lapply(as.list(rep("B", p)), function(x) eval(as.symbol(x))))
+    B <- Matrix::bdiag(lapply(as.list(rep("B", p)),
+                              function(x) eval(as.symbol(x))))
     # block diagonal of DB - p times
-    DB <- Matrix::bdiag(lapply(as.list(rep("DB", p)), function(x) eval(as.symbol(x))))
+    DB <- Matrix::bdiag(lapply(as.list(rep("DB", p)),
+                               function(x) eval(as.symbol(x))))
 
     xcoef <- aperm(xfd$coef, c(2, 1, 3)) # change dim- n X nbasis X p
     xcoef <- matrix(xcoef, nrow = n)
@@ -99,7 +106,8 @@ sonf = function(y, xfd, dev2_penalty = FALSE, lambda=NULL) {
     if (is.null(lambda)) {
       stop("lambda must be provided when dev2_penalty is TRUE")
     }
-    beta_coef <- MASS::ginv(as.matrix(t(X) %*% X + lambda * DB)) %*% t(X) %*% y_cen
+    beta_coef <- MASS::ginv(as.matrix(t(X) %*% X + lambda * DB)) %*%
+      t(X) %*% y_cen
   }
   pred <- X %*% beta_coef + my
   if (p == 1) {
