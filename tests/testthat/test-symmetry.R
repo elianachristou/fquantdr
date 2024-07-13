cf5a4059e5c49e63b3f0dc9ab50e3d8753b38169
Noah <noahpfd23@gmail.com>
2024-07-13 12:40:41 -0400

    Additional test of symmetry to make sure the input is a squre matrix.

--- tests/testthat/test-symmetry.R
+++ tests/testthat/test-symmetry.R
@@ -4,3 +4,8 @@ test_that("The input is a matrix", {
   sym.mat <- 3
   expect_error(symmetry(sym.mat))
 })
+
+test_that("The matrix is square", {
+  sym.mat <- matrix(rnorm(6), nrow = 2, ncol = 3)
+  expect_error(symmetry(sym.mat))
+})
