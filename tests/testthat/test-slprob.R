99758e43cc694e95422cdeac5af6cce26dce6c84
Eliana Christou <echris15@uncc.edu>
2024-09-03 13:29:06 -0400

    update testing for slprob

--- tests/testthat/test-slprob.R
+++ tests/testthat/test-slprob.R
@@ -1,15 +1,7 @@
 library(fquantdr)
 
-test_that("the function returns an error when y is not a vector", {
-  y <- -10000
-  # This statement should throw an error
-  H <- 3
-  expect_no_error(slprob(y, H))
-})
-
-test_that("H has to be a positive integer", {
-  y <- c(4, 7, 8, 9, 17)
-  # H also cannot contain a vector
+test_that("H is a positive integer", {
+  ydis <- c(4, 7, 8, 9, 17)
   H <- -3
-  expect_error(slprob(y, H))
+  expect_error(slprob(ydis, H))
 })
