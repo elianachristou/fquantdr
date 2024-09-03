1a0e874af1e78abaa9ac90215f5be92fcbf4741a
Eliana Christou <echris15@uncc.edu>
2024-09-03 14:02:23 -0400

    update testing for sonf

--- tests/testthat/test-sonf.R
+++ tests/testthat/test-sonf.R
@@ -1,20 +1,17 @@
 library(fquantdr)
 
 test_that("y is a univariate response", {
-  set.seed(1)
   n <- 100
   nbasis <- 10
   p <- 3
   basis <- fda::create.bspline.basis(rangeval = c(0, 1), nbasis = nbasis)
   coef_matrix <- array(rnorm(n * nbasis * p), dim = c(nbasis, n, p))
   xfd <- fda::fd(coef_matrix, basis)
-  # Creates a multivariate vector
   y <- matrix(1:6, nrow = 6, ncol = 1)
   expect_error(sonf(y, xfd))
 })
 
 test_that("y is of length n", {
-  set.seed(2)
   n <- 100
   m <- 80
   nbasis <- 8
@@ -27,7 +24,6 @@ test_that("y is of length n", {
 })
 
 test_that("xfd is a functional data object of the class 'fd'", {
-  set.seed(3)
   n <- 100
   nbasis <- 12
   p <- 3
@@ -37,13 +33,12 @@ test_that("xfd is a functional data object of the class 'fd'", {
   expect_error(sonf(y, coef_matrix))
 })
 
-test_that("xfd is a three dimensional array", {
-  set.seed(4)
+test_that("xfd is a 3-dimensional array", {
   n <- 100
   nbasis <- 7
   p <- 2
   basis <- fda::create.bspline.basis(rangeval = c(0, 1), nbasis = nbasis)
-  coef_matrix <- array(rnorm(n * nbasis * p), dim = c(nbasis, n))
+  coef_matrix <- array(rnorm(n * nbasis), dim = c(nbasis, n))
   xfd <- fda::fd(coef_matrix, basis)
   y <- rnorm(n)
   expect_error(sonf(y, xfd))
