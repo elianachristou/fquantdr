301116819cf607405e338860556e96c6e4271cd3
Eliana Christou <echris15@uncc.edu>
2025-03-07 11:32:25 -0500

    add the initial code for the function and add the name of the function on the NAMESPACE

--- R/fpca.R
+++ R/fpca.R
@@ -20,4 +20,52 @@
 #' @examples
 #'
 #' @export
+fpca <- function(ftn, basisname) {
+  # Extract coefficient array from input 'ftn' and determine its dimensions
+  temp <- ftn$coef
+  n <- dim(temp)[2]
+  p <- dim(temp)[3]
+  nt <- dim(temp)[1]
 
+  if(is.na(p)) p <- 1
+  xcoef <- temp
+
+  basis <- ftn$basis
+  # Generate basis matrix based on specified basis type
+  if(basisname == 'bspline') {
+    GB <- bsplinepen(basis, Lfdobj = 0)
+  }
+  else if(basisname == 'fourier') {
+    GB <- fourierpen(basis, Lfdobj = 0)
+  }
+
+  # Centering matrix for FPCA
+  one <- matrix(1, n, 1)
+  Q <- diag(n) - one %*% t(one) / n
+  # Compute the square root of the penalty matrix
+  B.half <- matpower(GB, 0.5)
+
+  if(p==1) {
+    Sigma <- B.half %*% xcoef %*% Q %*% t(xcoef) %*% B.half / n
+    egn <- eigen(Sigma, sym = TRUE)
+    B.inv.half <- matpower(GB, -0.5)
+    pred <- Q %*% t(xcoef) %*% GB %*% B.inv.half %*% egn$vec
+    out <- list(pred = pred, eval = egn$val, mat = B.inv.half %*% egn$vec)
+  } else if(p > 1) { # BX is the same for now...
+    M.half <- B.half %*% xcoef[, , 1] %*% Q
+    B.inv.half <- matpower(GB, -0.5)
+    D.inv.half <- B.inv.half
+    BB <- GB %*% xcoef[, , 1] %*% Q
+    for(j in 2:p) {
+      D.inv.half <- as.matrix(bdiag2(D.inv.half, B.inv.half))
+      M.half <- rbind(M.half, B.half %*% xcoef[, , j] %*% Q)
+      BB <- rbind(BB, GB %*% xcoef[, , j] %*% Q)
+    }
+    DD <- M.half
+    M.half <- M.half %*% t(M.half) / n
+    egn <- eigen(M.half, sym = TRUE)
+    pred <- t(BB) %*% D.inv.half %*% egn$vec
+    out <- list(pred = pred, eval = egn$val, mat = D.inv.half %*% egn$vec)
+  }
+  return(out)
+}
