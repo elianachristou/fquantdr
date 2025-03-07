121899a8338d131b2ed2fbc8a4af0cfaa40af8fe
Eliana Christou <echris15@uncc.edu>
2025-03-07 11:26:38 -0500

    define the fpca R file and write the input parameters

---
+++ R/fpca.R
@@ -0,0 +1,12 @@
+#' Functional Principal Component Analysis
+#'
+#' \code{fpca} performs functional principal component analysis (FPCA) on functional
+#' data.
+#'
+#' This function performs FPCA on the given functional data and returns the
+#' eigenvalues and eigenfunctions of the covariance matrix.
+#'
+#' @param ftn A list containing the coefficients of the functional object and
+#'    the basis
+#' @param basisname A string specifying the type of basis function to use for FPCA
+#'
