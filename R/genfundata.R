47f4c8218e0da908d2df659deddccfe897d2d197
Eliana Christou <echris15@uncc.edu>
2025-03-07 12:48:47 -0500

    add the documentation: inputs, outputs, and details

--- R/genfundata.R
+++ R/genfundata.R
@@ -1,33 +1,27 @@
 #' Functional data
 #'
-#' \code{fundata} generates functional data based on basis functions.
+#' \code{fundata} generates functional data using either B-spline or Fourier
+#' basis functions.
 #'
-#' This function generates functional data for a given number of observations
-#' ('n'), functional predictors ('p'), basis functions ('nbasis'), and time
-#' points ('time').  It utilizes a specified coefficient matrix ('eta') to
-#' create the functional predictors.  Using these basis functions, it computes
-#' the original functional predictors ('x') by multiplying the coefficients
-#' with the basis functions. The function then centers these functional
-#' predictors ('xc') by subtracting the mean across observations for each time
-#' point.  The output is a list containing both the original and centered
-#' functional predictors.
+#' This function constructs functional data using basis expansions.  The
+#' coefficients for the basis functions are randomly generated, and an
+#' orthogonalization step is applied using a Q-matrix.  Functional principal
+#' component analysis (FPCA) is then performed to obtain a low-dimensional
+#' representation of the data.
 #'
 #' @param n The number of observations, i.e., sample size.
 #' @param p The number of functional predictors.
-#' @param nbasis The number of Fourier basis functions.
-#' @param time A vector of time points at which the functional data is
+#' @param nbasis The number of basis functions.
+#' @param tt A vector of time points at which the functional data is
 #'     evaluated.
-#' @param eta A matrix of coefficients with dimensions \code{n} by
-#'     \code{p * nbasis}.
+#' @param basisname A character string for the type of basis functions to use,
+#'     either 'bspline' or 'fourier'.  Default is 'bspline'.
 #'
-#' @return \code{fundata} generates functional data based on Fourier
-#'    basis functions and returns:
+#' @return \code{genfundata} returns a list containing:
 #'    \itemize{
-#'        \item \code{x}: The original functional predictors, a
+#'        \item \code{X}: An array of generated functional data of dimensions
 #'            \code{n * nt * p} array, where \code{nt} denotes the number
 #'            of time points.
-#'        \item \code{xc}: The centered functional predictors, a
-#'            \code{n * nt * p} array, where \code{nt} denotes the number
-#'            of time points.
+#'        \item \code{mfpca.scores}: Matrix of FPCA scores.
 #'    }
 #'
