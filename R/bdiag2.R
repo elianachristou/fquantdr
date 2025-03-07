9299ffe7fe14850d5e45d96e4b788a4b4c8aa8e1
Eliana Christou <echris15@uncc.edu>
2025-03-07 13:39:25 -0500

    typo

--- R/bdiag2.R
+++ R/bdiag2.R
@@ -26,9 +26,11 @@ bdiag2 <- function(M, N) {
 
   # Initialize an empty matrix with appropriate size
   out <- matrix(0, nr + nr2, nc + nc2)
-  # Pllace M in the top-left block
+
+  # Place M in the top-left block
   out[(1:nr), (1:nc)] <- M
   # Place N in the bottom-right block
   out[((nr + 1):(nr + nr2)), ((nc + 1):(nc + nc2))] <- N
+
   return(out)
 }
