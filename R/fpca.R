4b8834677becc1c0229847f841746e0c5b76369d
Eliana Christou <echris15@uncc.edu>
2025-03-07 11:29:08 -0500

    write the output, i.e., what the function returns

--- R/fpca.R
+++ R/fpca.R
@@ -10,3 +10,9 @@
 #'    the basis
 #' @param basisname A string specifying the type of basis function to use for FPCA
 #'
+#' @return \code{fpca} returns:
+#'    \itemize{
+#'        \item \code{pred}: The principal component scores matrix
+#'        \item \code{eval}: The eigenvalues
+#'        \item \code{mat}: The matrix of eigenfunctions
+#'    }
