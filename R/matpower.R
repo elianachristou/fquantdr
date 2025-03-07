c7140fd0f43e2940d8a06390baa2e03e9fcb8c41
Eliana Christou <echris15@uncc.edu>
2025-03-07 13:59:04 -0500

    update documentation and add one more example

--- R/matpower.R
+++ R/matpower.R
@@ -1,6 +1,6 @@
 #' Matrix power transformation
 #'
-#' \code{matpower} computes the power of a square matrix using eigen
+#' \code{matpower} computes the power of a square matrix using its eigen
 #'     decomposition.
 #'
 #' This function takes a square matrix \code{a} and an exponent \code{alpha}
@@ -9,17 +9,23 @@
 #' eigenvalues to the power of \code{alpha}, and then reconstructs the
 #' matrix using the transformed eigenvectors and adjusted eigenvalues.
 #' The resulting matrix is symmetric (rounded to five decimal places).
+#' If \code{alpha} = 1, the function simply returns \code{a}.
 #'
-#' @param a The input square matrix.
+#' @param a A square numeric matrix.
 #' @param alpha The exponent to which the matrix is raised.
 #'
 #' @return The square matrix raised to the power of \code{alpha}.
 #'
 #' @noRd
 #' @examples
+#' Example 1: Square matrix raised to the power of 2
 #' mat <- matrix(c(6, 4, 8, 2, 5, 9, 3, 1, 7), nrow = 3, ncol = 3)
 #' alpha <- 2
 #' matpower(mat, alpha)
+#'
+#' Example 2: Computing the square root of a positive definite matrix
+#' pos_def_mat <- matrix(c(4, 1, 1, 3), nrow = 2, ncol = 2)
+#' matpower(pos_def_mat, 0.5)
 #'
 matpower <- function(a, alpha) {
 
@@ -33,17 +39,12 @@ matpower <- function(a, alpha) {
     stop("The input 'a' must be a square matrix.")
   }
 
-  # Check if 'alpha' is a single number
-  if (length(alpha) != 1) {
-    stop("The exponent 'alpha' must be a one-dimensional scalar.")
+  # Check if 'alpha' is a single numeric value
+  if (length(alpha) != 1 || !is.numeric(alpha)) {
+    stop("The exponent 'alpha' must be a numeric scalar.")
   }
 
-  # Check if 'alpha' is numeric
-  if (!is.numeric(alpha)) {
-    stop("The exponent 'alpha' must be numeric. ")
-  }
-
-  # Symmetrize the input matrix 'a'
+  # Symmetrize the input matrix 'a' for numerical stability
   a <- round((a + t(a)) / 2, 5)
 
   # Compute eigen decomposition
@@ -53,6 +54,6 @@ matpower <- function(a, alpha) {
   # decomposition
   result <- tmp$vectors %*% diag((tmp$values)^alpha) %*%
            t(tmp$vectors)
-  result
+  return(result)
 }
 
